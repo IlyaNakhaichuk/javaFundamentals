246f9c1a0ded5fbb54d302c16c54b3ffbab672a4
Ilya Nakhaichuk <ilya.nakhaichuk@gmail.com>
2019-07-12 16:48:39 +0300

    update optionalTask2

--- src/by/epam/task/fundamentals/optionalTask2/ConsoleMain.java
+++ src/by/epam/task/fundamentals/optionalTask2/ConsoleMain.java
@@ -2,6 +2,12 @@ package by.epam.task.fundamentals.optionalTask2;
 
 public class ConsoleMain {
     public static void main(String[] args) {
-        ConsoleTreatment.searchMinAndMaxNumber(args);
+        new ConsoleTreatment() {
+            {
+                this.searchMinAndMaxNumber( args );
+                this.outputNumberInAscendingAndDescendingOrder( args );
+                this.outputNumberLessThanTheAverageLength( args );
+            }
+        };
     }
 }
